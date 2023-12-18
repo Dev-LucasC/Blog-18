79499d2b164ab354481781f645aad2ab528d309c
Dev-LucasC <lucaconceicao@hotmail.com>
2023-12-18 15:32:31 -0300

    Paginação

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-    //
+    public function index()
+    {
+        $posts = Post::with(['user', 'comments'])->paginate(4);
+        return view('posts', ['title' => 'Posts', 'posts' => $posts]);
+        dd($posts);
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\PostsController;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
@@ -11,5 +12,5 @@ Route::get('/post/{post:slug}', [PostController::class, 'show'])->name('post');
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'store'])->name('login.store');
 Route::get('/logout', [LoginController::class, 'destroy'])->name('logout');
-Route::get('/posts', [PostsController::class, 'inex'])->name('posts');
+Route::get('/posts', [PostsController::class, 'index'])->name('posts');
 
